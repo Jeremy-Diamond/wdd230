38889bc765125413cc06a1775ba313d4b3ab9e03
Jeremy Diamond <socjeremyd@gmail.com>
2022-01-07 21:53:05 -0700

    Week 1 Changes

--- js/getdates.js
+++ js/getdates.js
@@ -3,5 +3,5 @@
 const options = {weekday: 'long', day:'numeric', month: 'long', year: 'numeric'};
 const todaysDate = new Date()
 
-document.querySelector("#currentYear").textContent = todaysDate.getFullYear();
+document.querySelector("#currentYear").textContent = `©${todaysDate.getFullYear()}`;
 document.querySelector("#lastUpdated").textContent = `Last Updated: ${document.lastModified}`;

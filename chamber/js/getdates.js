6f93b0258abf29ea491586713e5c67ffa6f593f1
Jeremy Diamond <socjeremyd@gmail.com>
2022-01-29 23:07:59 -0700

    chamber site v1

---
+++ chamber/js/getdates.js
@@ -0,0 +1,7 @@
+// Easy Version
+
+const options = {weekday: 'long', day:'numeric', month: 'long', year: 'numeric'};
+const todaysDate = new Date()
+
+document.querySelector("#currentYear").textContent = `©${todaysDate.getFullYear()}`;
+document.querySelector("#lastUpdated").textContent = `Last Updated: ${document.lastModified}`;
---
+++ chamber/js/hambugerScripts.js
@@ -0,0 +1,12 @@
+
+
+document.querySelector('.hamburgerBtn').innerHTML="test";
+/*
+const primaryNav = document.querySelector('.primaryNav')
+
+hambutton.addEventListener('click', () => {primaryNav.classList.toggle('responsive')}, false);
+
+// To solve the mid resizing issue with responsive class on
+window.onresize = () => {if (window.innerWidth > 760) primaryNav.classList.remove('responsive')};
+*/
+

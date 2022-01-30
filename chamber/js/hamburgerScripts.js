c475dc6e9b9d54011a4827b1bba7594ae2442ad1
Jeremy Diamond <socjeremyd@gmail.com>
2022-01-30 01:10:40 -0700

    chamber updates

--- chamber/js/hamburgerScripts.js
+++ chamber/js/hamburgerScripts.js
@@ -1,12 +1,9 @@
 
 
-document.querySelector('.hamburgerBtn').innerHTML="test";
-/*
+const hamburgerBtn = document.querySelector('.hambugerBtn');
 const primaryNav = document.querySelector('.primaryNav')
 
-hambutton.addEventListener('click', () => {primaryNav.classList.toggle('responsive')}, false);
+hamburgerBtn.addEventListener('click', () => {primaryNav.classList.toggle('responsive')}, false);
 
 // To solve the mid resizing issue with responsive class on
 window.onresize = () => {if (window.innerWidth > 760) primaryNav.classList.remove('responsive')};
-*/
-

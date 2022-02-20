17ec7325bf200b12553ea0257d3868e655cb38ef
Jeremy Diamond <socjeremyd@gmail.com>
2022-02-19 17:17:59 -0700

    adding Missing you js

---
+++ chamber/js/localstorage.js
@@ -0,0 +1,38 @@
+
+
+
+const lastvisit = localStorage.getItem('lastvisit');
+
+const SECONDS_FACTOR = 1000;
+const MINUTES_FACTOR = SECONDS_FACTOR * 60;
+const HOURS_FACTOR = MINUTES_FACTOR * 60;
+const DAYS_FACTOR = HOURS_FACTOR * 24;
+
+let daysbetween = Date.now() - lastvisit;
+
+let numberofseconds = Math.round(daysbetween / SECONDS_FACTOR);
+let numberofminutes =  Math.round(daysbetween / MINUTES_FACTOR);
+let numberofhours = Math.round(daysbetween / HOURS_FACTOR);
+let numberofdays = Math.round( daysbetween / DAYS_FACTOR);
+
+
+
+console.log(`days ${numberofdays}`)
+console.log(`hours ${numberofhours}`)
+console.log(`minutes ${numberofminutes}`)
+console.log(`seconds ${numberofseconds}`)
+console.log(`daysbetween ${daysbetween >= 1}`)
+
+if(lastvisit === null) {
+    document.querySelector(".dayssincelastvisit").textContent = "Welcome to the Diamond City Discover Page!"
+} else if (numberofdays >= 1) {
+        document.querySelector(".dayssincelastvisit").textContent = `Welcom back! It has been ${numberofdays} days since we have seen you.`
+    }else if(numberofhours >= 1){
+        document.querySelector(".dayssincelastvisit").textContent = `I knew you would only make it ${numberofhours} hours before needing to come back!`
+    }else if (numberofminutes >= 1){
+        document.querySelector(".dayssincelastvisit").textContent = `I was sad when you left, but I didn't know it was only going to be ${numberofminutes} minutes!`
+    }else{
+        document.querySelector(".dayssincelastvisit").textContent = `That is a new record! You couldn't stay away for more than ${numberofseconds} seconds`
+    }
+
+localStorage.setItem("lastvisit",Date.now());
